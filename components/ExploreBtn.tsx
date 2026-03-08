5fc037bce0c7635134bd4226f9993d6ac997158b
phong.tran <phong.tran-ost@asiapac.com.sg>
2026-03-08 13:06:27 +0700

    Implement posthog

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -2,10 +2,16 @@
 'use client'
 import Image from 'next/image'
 import React from 'react'
+import posthog from 'posthog-js'
 
 const ExploreBtn = () => {
+  const handleClick = () => {
+    console.log('Explore button clicked')
+    posthog.capture('explore_events_clicked')
+  }
+
   return (
-    <button type='button' id='explore-btn' className='mt-7 mx-auto' onClick={() => {console.log('Explore button clicked')}}>
+    <button type='button' id='explore-btn' className='mt-7 mx-auto' onClick={handleClick}>
         <a href="#events">
             Explore Events
             <Image src={"/icons/arrow-down.svg"} alt='Arrow Down' width={24} height={24}/>
